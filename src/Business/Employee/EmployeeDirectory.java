407d48e0197b0b9c66084dee539c538c8df65254
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2022-12-09 05:20:46 -0500

    Fixed indentations for proper reading of code

--- src/Business/Employee/EmployeeDirectory.java
+++ src/Business/Employee/EmployeeDirectory.java
@@ -14,28 +14,38 @@ public class EmployeeDirectory {
 
     private ArrayList<Employee> employeeList;
 
+
     public EmployeeDirectory() {
-        employeeList = new ArrayList();
+        employeeList=new ArrayList();
+
     }
 
     public ArrayList<Employee> getEmployeeList() {
-        return employeeList;
+       
+ return employeeList;
     }
 
+
     public Employee createEmployee(String name) {
+
         Employee employee = new Employee();
+
         employee.setName(name);
         employeeList.add(employee);
+
         return employee;
     }
     
     public void removeEmployee(Employee employee) {
+
         employeeList.remove(employee);
     }
 
     public Employee uniqueEmployee(String name) {
-        for (Employee em : employeeList) {
-            if (em.getName().equals(name)) {
+
+        for(Employee em : employeeList) {
+           
+ if (em.getName().equals(name)) {
                 return em;
             }
         }
